5cdca7bceef59296989a9fb7bf0d3c279c8bcbdc
Amit <tech_fervour@yahoo.com>
2021-03-10 08:17:28 +0530

    added few comments

--- react-frontend/src/drizzleOptions.js
+++ react-frontend/src/drizzleOptions.js
@@ -1,5 +1,8 @@
+// importing our contracts artifact file
 import DisplayTagline from "./contracts/DisplayTagline.json"
 
+// options to use like which network to connect
+// which contract to use, etc.
 const options = {
   web3: {
     block: false,
